0eeb1429285d806a4e7aaa3b8f9315b0eddd1b10
stefanbirs <31699407+stefanbirs@users.noreply.github.com>
2018-10-12 14:14:33 +0200

    Created grip script

---
+++ PopUpNext/grid.py
@@ -0,0 +1,14 @@
+width = int(input("How wide?"))
+height = int(input("How high?"))
+bak = "."
+grid = []
+row = []
+for i in range(5):
+    grid.append(row)
+for li in grid:
+    print(id(li))
+# 92532104
+# 92532104
+# 92532104
+# 92532104
+# 92532104

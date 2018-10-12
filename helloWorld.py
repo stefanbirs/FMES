3de6bcbc4b316d2c3197527695b3ea864dc6d2b4
kamranthomas <kalima15@student.aau.dk>
2018-10-12 11:37:07 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 import random
 
-=======
-import random as rd
->>>>>>> origin/master
 print("Hello world!");
 for i in range(5):
 
-<<<<<<< HEAD
     print(random.randrange(1,20))
-=======
-
-    print(rd.randint(1,6))
->>>>>>> origin/master

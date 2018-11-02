2dd7a26f85d52a78b3a8ce46700ba4b6d226d9d6
Hezzeldezzel <m.hesseldahl@gmail.com>
2018-11-02 11:35:13 +0100

    Change og constants

--- PopUpNext/window.py
+++ PopUpNext/window.py
@@ -24,21 +24,18 @@ canvas.pack()
 canvas.configure(highlightthickness=0, borderwidth=0) # removes cut off on top and left edges
 
 # Road width
-rd= 20
-
+RD= 20
 # house width
-hw=int((WIDTH-(((len(maze)/2)+0.5)*rd))/((len(maze)/2)-0.5))
+HW=int((WIDTH-(((len(maze)/2)+0.5)*RD))/((len(maze)/2)-0.5))
 
 
 """ House """
 # Makes houses for every second coordinate in the x and y axis
-boxx=0
 for i in range(int((len(tmaze)-1)/2)):
-    boxy=0
     for i in range(int((len(tmaze[0])-1)/2)):
-        canvas.create_rectangle(rd+boxx,rd+boxy,rd+hw+boxx,rd+hw+boxy)
-        boxy=boxy+hw+rd
-    boxx=boxx+hw+rd
+        canvas.create_rectangle(RD+boxx,RD+boxy,RD+HW+boxx,RD+HW+boxy)
+        boxy=boxy+HW+RD
+    boxx=boxx+HW+RD
 
 
 """Traffic"""
@@ -46,23 +43,23 @@ for i in range(int((len(tmaze)-1)/2)):
 for x in range(len(tmaze)):
     for y in range(len(tmaze[0])):
         if tmaze[x][y] == 1 and not x % 2 == 0: #Makes a rectangle between buildings in the x axis
-            column=((hw+rd)*((x/2)-0.5))+rd
-            row=(hw+rd)*(y/2)
-            canvas.create_rectangle(column,row ,hw+column , 20+row, fill="red")
+            column=((HW+RD)*((x/2)-0.5))+RD
+            row=(HW+RD)*(y/2)
+            canvas.create_rectangle(column,row ,HW+column , 20+row, fill="red")
         if tmaze[x][y] == 1 and not y % 2 == 0: #Makes a rectangle between buildings in the y axis
-            row=((hw+rd)*((y/2)-0.5))+rd
-            column=(hw+rd)*(x/2)
-            canvas.create_rectangle(column,row , 20+column , hw+row, fill="red")
+            row=((HW+RD)*((y/2)-0.5))+RD
+            column=(HW+RD)*(x/2)
+            canvas.create_rectangle(column,row , 20+column , HW+row, fill="red")
         if tmaze[x][y] == 1 and x % 2 == 0 and y % 2 == 0 : #Makes a squere on crossroads
-            row=((hw+rd)*(y/2))
-            column=((hw+rd)*(x/2))
+            row=((HW+RD)*(y/2))
+            column=((HW+RD)*(x/2))
             canvas.create_rectangle(column,row , 20+column , row+20, fill="red")
 
 
 
 """Goal"""
 # Creates a squere at the end coordinate
-canvas.create_rectangle(int(end[0]*((hw+rd)/2)),int(end[1]*((hw+rd)/2)) ,int(20+end[0]*((hw+rd)/2)),int(20+end[1]*((hw+rd)/2)), fill="blue")
+canvas.create_rectangle(int(end[0]*((HW+RD)/2)),int(end[1]*((HW+RD)/2)) ,int(20+end[0]*((HW+RD)/2)),int(20+end[1]*((HW+RD)/2)), fill="blue")
 
 """ Wheels """
 # Since the wheels is moving this function is called every time the canvas is updated
@@ -70,7 +67,7 @@ class Wheels:
     # Defines all the varibles we are using in the function
     def __init__(self,color, size): #(self, color of wheels, size of squere)
         # This is defining the start position of the wheels
-        self.shape= canvas.create_rectangle(int(start[0]*((hw+rd)/2)),int(start[1]*((hw+rd)/2)),int(start[0]*((hw+rd)/2)+size),int(start[1]*((hw+rd)/2)+size), fill=color)
+        self.shape= canvas.create_rectangle(int(start[0]*((HW+RD)/2)),int(start[1]*((HW+RD)/2)),int(start[0]*((HW+RD)/2)+size),int(start[1]*((HW+RD)/2)+size), fill=color)
         # The speed of the wheels
         self.xspeed = 0
         self.yspeed = 0
@@ -90,8 +87,8 @@ class Wheels:
         pos= canvas.coords(self.shape)
 
         #indentify the end coordinates
-        slutx=int(end[0]*((hw+rd)/2))
-        sluty=int(end[1]*((hw+rd)/2))
+        slutx=int(end[0]*((HW+RD)/2))
+        sluty=int(end[1]*((HW+RD)/2))
 
         if (pos[0],pos[1]) == (slutx,sluty) : #It only enters this statement if its at the end point
             # It stops the wheel
@@ -144,25 +141,25 @@ class Wheels:
         # This is done to make the if statements above to look at the next step of the path
 
         # looks if the wheel has reached a new coordinate in the positive x direction
-        if pos[0] >=((hw+rd)/2)*path[self.b][self.x] and path[self.a][self.y] == path[self.b][self.y] and self.i==0:
+        if pos[0] >=((HW+RD)/2)*path[self.b][self.x] and path[self.a][self.y] == path[self.b][self.y] and self.i==0:
             self.xspeed = 0
             self.a = self.a + 1
             self.b = self.b + 1
             self.i=3
         # looks if the wheel has reached a new coordinate in the negative x direction
-        if pos[0] <=((hw+rd)/2)*path[self.b][self.x] and path[self.a][self.y] == path[self.b][self.y] and self.i==1:
+        if pos[0] <=((HW+RD)/2)*path[self.b][self.x] and path[self.a][self.y] == path[self.b][self.y] and self.i==1:
             self.xspeed = 0
             self.a = self.a + 1
             self.b = self.b + 1
             self.i=3
         # looks if the wheel has reached a new coordinate in the positive y direction
-        if pos[1] >=((hw+rd)/2)*path[self.b][self.y] and path[self.a][self.x] == path[self.b][self.x] and self.i==0:
+        if pos[1] >=((HW+RD)/2)*path[self.b][self.y] and path[self.a][self.x] == path[self.b][self.x] and self.i==0:
             self.yspeed = 0
             self.a = self.a + 1
             self.b = self.b + 1
             self.i=3
         # looks if the wheel has reached a new coordinate in the negative y direction
-        if pos[1] <=((hw+rd)/2)*path[self.b][self.y] and path[self.a][self.x] == path[self.b][self.x] and self.i==1:
+        if pos[1] <=((HW+RD)/2)*path[self.b][self.y] and path[self.a][self.x] == path[self.b][self.x] and self.i==1:
             self.yspeed = 0
             self.a = self.a + 1
             self.b = self.b + 1

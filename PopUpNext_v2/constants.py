135c7ba0b548f8d797da3b2b341aae7e70093dbf
kamranthomas <kalima15@student.aau.dk>
2018-11-10 21:55:41 +0100

    splitting up files

---
+++ PopUpNext_v2/constants.py
@@ -0,0 +1,13 @@
+import parameters as param
+# Canvas
+CWIDTH = 700 # CANVAS WIDTH
+CHEIGHT = 700 # CANVAS LENGTH
+#
+RWITDH = 20 # ROAD WIDTH
+RLENGTH = int( (CWIDTH-(((len(param.maze)/2)+0.5)*RWITDH)) / ((len(param.maze)/2)-0.5) ) # ROAD LENGTH
+SHAPE_SIZE = RWITDH
+#
+TMAZE_COLS = len(param.tmaze) # number of columns
+TMAZE_ROWS = len(param.tmaze[0]) # number of rows
+#
+NUM_OF_WHEELS = 3

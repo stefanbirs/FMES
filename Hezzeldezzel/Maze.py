2e6d4837f719cdc2c60bb6a41ac360bec1cfd512
Hezzeldezzel <m.hesseldahl@gmail.com>
2018-11-16 12:40:37 +0100

    Trafic

--- Hezzeldezzel/Maze.py
+++ Hezzeldezzel/Maze.py
@@ -12,19 +12,19 @@ canvas.pack()
 
 canvas.create_rectangle(0,0,WIDTH,HEIGHT)
 
-amaze = [[0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+amaze = [[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 2, 1, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 2, 1, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
-        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 2, 0, 2, 1, 2, 0, 2, 0, 2, 0, 2, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 2, 0, 2, 1, 2, 0, 2, 0, 2, 0, 2, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
 
 rd= 20
 
@@ -35,19 +35,47 @@ s = (len(amaze),len(amaze[0]))
 Trafmaze=np.zeros(s)
 NoTrafmaze=np.zeros(s)
 
+a=[]
+b=[]
+
+for i in range(len(amaze)):
+    for j in range(len(amaze[0])):
+        if amaze[i][j] != 2 and not i % 2 == 0:
+            column=((hw+rd)*((i/2)-0.5))+rd
+            row=(hw+rd)*(j/2)
+            a.append(canvas.create_rectangle(column,row ,hw+column , 20+row, fill=""))
+            b.append(i)
+            b.append(j)
+        if amaze[i][j] != 2 and not j % 2 == 0:
+            row=((hw+rd)*((j/2)-0.5))+rd
+            column=(hw+rd)*(i/2)
+            a.append(canvas.create_rectangle(column,row , 20+column , hw+row, fill=""))
+            b.append(i)
+            b.append(j)
+        if amaze[i][j] != 2 and i % 2 == 0 and j % 2 == 0 :
+            row=((hw+rd)*(j/2))
+            column=((hw+rd)*(i/2))
+            a.append(canvas.create_rectangle(column,row , 20+column , row+20, fill=""))
+            b.append(i)
+            b.append(j)
+        if amaze[i][j]==2:
+            a.append(0)
+            b.append(i)
+            b.append(j)
+print(b)
 while True :
 
     for i in range(len(Trafmaze)):
         for j in range(len(Trafmaze)):
             if amaze[i][j]==1:
-                Trafmaze[i][j]=random.randrange(1,20)
+                Trafmaze[i][j]=random.randrange(1,2000)
             if Trafmaze[i][j]>0:
                 Trafmaze[i][j]=Trafmaze[i][j]-1
             if Trafmaze[i][j]==1:
                 amaze[i][j]=0
                 continue
             if amaze[i][j]==0:
-                NoTrafmaze[i][j]=random.randrange(1,20)
+                NoTrafmaze[i][j]=random.randrange(1,2000)
             if NoTrafmaze[i][j]>0:
                 NoTrafmaze[i][j]=NoTrafmaze[i][j]-1
             if NoTrafmaze[i][j]==1:
@@ -59,16 +87,28 @@ while True :
             if amaze[i][j] == 1 and not i % 2 == 0:
                 column=((hw+rd)*((i/2)-0.5))+rd
                 row=(hw+rd)*(j/2)
-                canvas.create_rectangle(column,row ,hw+column , 20+row, fill="red")
+                canvas.itemconfig(a[i+(j*13)], fill="red", outline="black")
             if amaze[i][j] == 1 and not j % 2 == 0:
                 row=((hw+rd)*((j/2)-0.5))+rd
                 column=(hw+rd)*(i/2)
-                canvas.create_rectangle(column,row , 20+column , hw+row, fill="red")
+                canvas.itemconfig(a[i+(j*13)], fill="red", outline="black")
             if amaze[i][j] == 1 and i % 2 == 0 and j % 2 == 0 :
                 row=((hw+rd)*(j/2))
                 column=((hw+rd)*(i/2))
-                canvas.create_rectangle(column,row , 20+column , row+20, fill="red")
-    print(amaze)
+                canvas.itemconfig(a[i+(j*13)], fill="red", outline="black")
+            if amaze[i][j] == 0 and not i % 2 == 0:
+                column=((hw+rd)*((i/2)-0.5))+rd
+                row=(hw+rd)*(j/2)
+                canvas.itemconfig(a[i+(j*13)], fill="", outline="")
+            if amaze[i][j] == 0 and not j % 2 == 0:
+                row=((hw+rd)*((j/2)-0.5))+rd
+                column=(hw+rd)*(i/2)
+                canvas.itemconfig(a[i+(j*13)], fill="", outline="")
+            if amaze[i][j] == 0 and i % 2 == 0 and j % 2 == 0 :
+                row=((hw+rd)*(j/2))
+                column=((hw+rd)*(i/2))
+                canvas.itemconfig(a[i+(j*13)], fill="", outline="")
+
     tk.update()
 
 tk.mainloop()

e03a3ffcdf517dd1bc294be9c8ec0f0af955bf55
Hezzeldezzel <m.hesseldahl@gmail.com>
2018-11-23 16:44:46 +0100

    Maze final trafic

--- Hezzeldezzel/Maze.py
+++ Hezzeldezzel/Maze.py
@@ -35,70 +35,49 @@ s = (len(amaze),len(amaze[0]))
 Trafmaze=np.zeros(s)
 NoTrafmaze=np.zeros(s)
 
-a=[]
-b=[]
+a = [[amaze[j][i] for j in range(len(amaze))] for i in range(len(amaze[0]))]
 
 for i in range(len(amaze)):
     for j in range(len(amaze[0])):
-        if amaze[i][j] != 2 and not i % 2 == 0:
-            column=((hw+rd)*((i/2)-0.5))+rd
-            row=(hw+rd)*(j/2)
-            a.append(canvas.create_rectangle(column,row ,hw+column , 20+row, fill=""))
         if amaze[i][j] != 2 and not j % 2 == 0:
-            row=((hw+rd)*((j/2)-0.5))+rd
-            column=(hw+rd)*(i/2)
-            a.append(canvas.create_rectangle(column,row , 20+column , hw+row, fill=""))
+            column=((hw+rd)*((j/2)-0.5))+rd
+            row=(hw+rd)*(i/2)
+            a[i][j]=canvas.create_rectangle(column,row ,hw+column , 20+row, fill="")
+        if amaze[i][j] != 2 and not i % 2 == 0:
+            row=((hw+rd)*((i/2)-0.5))+rd
+            column=(hw+rd)*(j/2)
+            a[i][j]=canvas.create_rectangle(column,row , 20+column , hw+row, fill="")
         if amaze[i][j] != 2 and i % 2 == 0 and j % 2 == 0 :
-            row=((hw+rd)*(j/2))
-            column=((hw+rd)*(i/2))
-            a.append(canvas.create_rectangle(column,row , 20+column , row+20, fill=""))
+            row=((hw+rd)*(i/2))
+            column=((hw+rd)*(j/2))
+            a[i][j]=canvas.create_rectangle(column,row , 20+column , row+20, fill="")
+            continue
         if amaze[i][j]==2:
-            a.append(0)
+            a[i][j]=0
 
+
+trafictime=[1000,2000,3000]
 while True :
-
-
-
     for i in range(len(Trafmaze)):
         for j in range(len(Trafmaze)):
-            trafchance=random.randrange(1,1000)
+            trafchance=random.randrange(1,10000)
             if amaze[i][j]==1 and Trafmaze[i][j]==0:
-                Trafmaze[i][j]=200
+                Trafmaze[i][j]=trafictime[random.randrange(0,3)]
             if Trafmaze[i][j]>0:
                 Trafmaze[i][j]=Trafmaze[i][j]-1
             if Trafmaze[i][j]==0:
                 amaze[i][j]=0
-
             if amaze[i][j]==0 and trafchance==1:
                 amaze[i][j]=1
 
-
-    for i in range(len(amaze)):
-        for j in range(len(amaze[0])):
-            if amaze[i][j] == 1 and not i % 2 == 0:
-                column=((hw+rd)*((i/2)-0.5))+rd
-                row=(hw+rd)*(j/2)
-                canvas.itemconfig(a[i+(j*13)], fill="red", outline="black")
-            if amaze[i][j] == 1 and not j % 2 == 0:
-                row=((hw+rd)*((j/2)-0.5))+rd
-                column=(hw+rd)*(i/2)
-                canvas.itemconfig(a[i+(j*13)], fill="red", outline="black")
-            if amaze[i][j] == 1 and i % 2 == 0 and j % 2 == 0 :
-                row=((hw+rd)*(j/2))
-                column=((hw+rd)*(i/2))
-                canvas.itemconfig(a[i+(j*13)], fill="red", outline="black")
-            if amaze[i][j] == 0 and not i % 2 == 0:
-                column=((hw+rd)*((i/2)-0.5))+rd
-                row=(hw+rd)*(j/2)
-                canvas.itemconfig(a[i+(j*13)], fill="", outline="")
-            if amaze[i][j] == 0 and not j % 2 == 0:
-                row=((hw+rd)*((j/2)-0.5))+rd
-                column=(hw+rd)*(i/2)
-                canvas.itemconfig(a[i+(j*13)], fill="", outline="")
-            if amaze[i][j] == 0 and i % 2 == 0 and j % 2 == 0 :
-                row=((hw+rd)*(j/2))
-                column=((hw+rd)*(i/2))
-                canvas.itemconfig(a[i+(j*13)], fill="", outline="")
+            if amaze[i][j] == 1 and Trafmaze[i][j]> trafictime[1] :
+                canvas.itemconfig(a[i][j], fill="red", outline="black")
+            if amaze[i][j] == 1 and Trafmaze[i][j]> trafictime[0] and Trafmaze[i][j]<= trafictime[1] :
+                canvas.itemconfig(a[i][j], fill="orange", outline="black")
+            if amaze[i][j] == 1 and Trafmaze[i][j]<=trafictime[0] :
+                canvas.itemconfig(a[i][j], fill="yellow", outline="black")
+            if amaze[i][j] == 0 :
+                canvas.itemconfig(a[i][j], fill="", outline="")
 
     tk.update()
 

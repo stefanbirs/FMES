cce283d6c36a15e7e8adad40bfa1be0359eef3d0
kamranthomas <kalima15@student.aau.dk>
2018-11-27 13:08:05 +0100

    comment

--- PopUpNext_v2/parameters.py
+++ PopUpNext_v2/parameters.py
@@ -17,9 +17,11 @@ maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0]]
 tmaze = a_star.flip_matrix(maze) # Flipped City Map
 
+
 eq_len = 25
 len_x = eq_len # maze length x
 len_y = eq_len # maze length y
 size = (len_x, len_y)
 
+# generates City Map with traffic density
 tmaze = generate_citymap.generate_city(size, (0.2))
